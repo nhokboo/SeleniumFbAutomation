ea344923c1a62f248835e64dda1fd95f9d648a29
Nícolas Pastorello <4770542+nicopastorello@users.noreply.github.com>
2019-03-26 12:35:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from sfpa import SeleniumFbAutomation
-import os, json, random, ConfigParser
+import os, json, random, ConfigParser, time
 
 config = ConfigParser.ConfigParser()
 config.read(os.path.abspath('config.ini'))
@@ -15,20 +15,19 @@ else:
 	email = config.get('Contas', 'email_2')
 	senha = config.get('Contas', 'senha_2')
 
-postagem 		= config.get('Postagem', 'postagem')
-lista_imagens 	= random.choice(json.loads(config.get('Postagem', 'imagens')))
-pagina  		= config.get('Postagem', 'pagina')
+postagem 	= config.get('Postagem', 'postagem')
+lista_imagens 	= json.loads(config.get('Postagem', 'imagens'))
+pagina  	= config.get('Postagem', 'pagina')
 lista_grupos 	= json.loads(config.get('Postagem', 'grupos'))
 
 sfpa.show_graph()
 sfpa.autenticar(email, senha)
 sfpa.post_profile(postagem, lista_imagens)
-sfpa.progress_bar(0.90)
+sfpa.progress_bar(2.40)
 sfpa.post_page(pagina , postagem, lista_imagens)
-sfpa.progress_bar(0.90)
+sfpa.progress_bar(3.60)
 
-for id_group in sfpa.remove_repetidos(lista_grupos):
-	sfpa.post_group(id_group, postagem, lista_imagens)
+for id_group in lista_grupos:
 	sfpa.requests_group(id_group)
-	sfpa.progress_bar(0.90)
+	sfpa.post_group(id_group, postagem, random.choice(lista_imagens))
 sfpa.finalizar()
